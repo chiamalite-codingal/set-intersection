2a9cc3b81235b00704aaf0a31ef3533a8fa5f0d7
chiamalite-codingal <chiamalitecodingal@gmail.com>
2024-11-22 17:35:52 +0100

    set intersection

---
+++ main.py
@@ -0,0 +1,8 @@
+set1 = {"red,pink"}
+set2 = {"blue,yellow"}
+#intersection - taking common from set1 and set2
+answer = set1.intersection(set2)
+print("Answer is:",answer)
+#union - combining set1 and set2
+answer = set1.union(set2)
+print("Answer is:",answer)
